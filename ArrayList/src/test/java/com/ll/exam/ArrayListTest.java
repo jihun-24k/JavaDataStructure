3679fbf5e1653ff7a9ff73dfd1477e7a73a2b6d7
jihun-24k <gi020477@gmail.com>
2022-07-15 11:14:56 +0900

    Feat: @BeforeEach 도입

--- ArrayList/src/test/java/com/ll/exam/ArrayListTest.java
+++ ArrayList/src/test/java/com/ll/exam/ArrayListTest.java
@@ -1,8 +1,15 @@
 package com.ll.exam;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class ArrayListTest {
+    private ArrayList arr;
+
+    @BeforeEach
+    void BeforeEach(){
+        arr = new ArrayList();
+    }
     @Test
     public void ArrayListTest(){
         ArrayList arrayList = new ArrayList();
@@ -10,13 +17,11 @@ public class ArrayListTest {
 
     @Test
     public void addTestInt(){
-        ArrayList arr = new ArrayList();
         arr.add(100);
     }
 
     @Test
     public void addTestString(){
-        ArrayList arr = new ArrayList();
         arr.add("안녕");
     }
 }
