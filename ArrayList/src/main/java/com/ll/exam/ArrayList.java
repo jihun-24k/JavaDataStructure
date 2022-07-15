3aea267982963b425f518f094bfa68a7b306359b
jihun-24k <gi020477@gmail.com>
2022-07-15 10:53:33 +0900

    Feat: ArrayList 클래스 생성

---
+++ ArrayList/src/main/java/com/ll/exam/ArrayList.java
@@ -0,0 +1,7 @@
+package com.ll.exam;
+
+public class ArrayList {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ ArrayList/src/test/java/com/ll/exam/ArrayListTest.java
@@ -0,0 +1,10 @@
+package com.ll.exam;
+
+import org.junit.jupiter.api.Test;
+
+public class ArrayListTest {
+    @Test
+    public void runTest(){
+        ArrayList arrayList = new ArrayList();
+    }
+}
