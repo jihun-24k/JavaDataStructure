78b5aa16dda7ba3a3c63939fbf6bfad0dfcf9538
jihun-24k <gi020477@gmail.com>
2022-07-15 14:05:22 +0900

    Feat: Main 내용 입력

--- ArrayList/src/main/java/com/ll/exam/Main.java
+++ ArrayList/src/main/java/com/ll/exam/Main.java
@@ -3,7 +3,71 @@ package com.ll.exam;
 
 class Main {
     public static void main(String[] args) {
+        ArrayList al = new ArrayList();
 
+        System.out.println("al.size() : " + al.size());
+        // 출력 => al.size() : 0
+
+        al.add(100);
+
+        System.out.println("al.get(0) : " + al.get(0));
+        // 출력 => al.get(0) : 100
+
+        al.add(200);
+        al.add(300);
+        // 출력 => 배열의 크기가 증가되었습니다. 2 => 4
+
+        System.out.println("al.size() : " + al.size());
+        // 출력 => al.size() : 3
+
+        System.out.println("al.get(1) : " + al.get(1));
+        // 출력 => al.get(1) : 200
+
+        al.remove(1);
+
+        System.out.println("al.size() : " + al.size());
+        // 출력 => al.size() : 2
+
+        System.out.println("al.get(1) : " + al.get(1));
+        // 출력 => al.get(1) : 300
+
+        al.add(400);
+        al.add(500);
+        al.add(600);
+        // 출력 => 배열의 크기가 증가되었습니다. 4 => 8
+
+        System.out.println("al.get(3) + al.get(4) : " + ((int)al.get(3) + (int)al.get(4)));
+        // 출력 => al.get(3) + al.get(4) : 1100
+
+        System.out.println("al.get(3).intValue() + al.get(4).intValue() : " + (((Integer)al.get(3)).intValue() + ((Integer)al.get(4)).intValue()));
+        // 출력 => al.get(3) + al.get(4) : 1100
+
+        al.showAllValues();
+        // 출력 =>
+		/*
+		== 전체 데이터 출력 ==
+		0 : 100
+		1 : 300
+		2 : 400
+		3 : 500
+		4 : 600
+		*/
+
+        al.addAt(700, 1);
+        al.addAt(750, 1);
+
+        al.showAllValues();
+        // 출력 =>
+		/*
+		== 전체 데이터 출력 ==
+		0 : 100
+		1 : 750
+		2 : 700
+		3 : 300
+		4 : 400
+		5 : 500
+		6 : 600
+		*/
     }
 }
 
@@ -64,7 +128,7 @@ class ArrayList{
     public void showAllValues() {
         System.out.println("== 전체 데이터 출력 ==");
         for (int i = 0; i<lastIndex; i++){
-            System.out.printf("%d : %d",i, datum[i]);
+            System.out.printf("%d : %d\n",i, datum[i]);
         }
     }
 }
