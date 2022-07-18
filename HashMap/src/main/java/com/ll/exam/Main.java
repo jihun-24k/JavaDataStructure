b63a2ed66968e02b927779cb194d9af3998b84c7
jihun-24k <gi020477@gmail.com>
2022-07-18 13:42:59 +0900

    Feat: HashMap 클래스 생성

---
+++ HashMap/src/main/java/com/ll/exam/Main.java
@@ -0,0 +1,11 @@
+package com.ll.exam;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
+
+class HashMap<K, V>{
+
+}
---
+++ HashMap/src/test/java/com/ll/exam/HashMapTest.java
@@ -0,0 +1,13 @@
+package com.ll.exam;
+
+import org.junit.jupiter.api.Test;
+
+
+public class HashMapTest {
+    @Test
+    void HashMapTest(){
+        HashMap<String, Integer> hs1= new HashMap<>();
+        HashMap<String, Object> hs2= new HashMap<>();
+    }
+
+}
