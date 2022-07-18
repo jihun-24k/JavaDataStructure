0f8a828a1adb16a0d9951d91dea8d5ad0fa580d6
jihun-24k <gi020477@gmail.com>
2022-07-18 14:04:52 +0900

    Feat: put 메서드 구현

--- HashMap/src/main/java/com/ll/exam/Main.java
+++ HashMap/src/main/java/com/ll/exam/Main.java
@@ -7,5 +7,19 @@ public class Main {
 }
 
 class HashMap<K, V>{
+    private Object[] keys;
+    private Object[] values;
+    private int size;
 
+    HashMap(){
+        this.size = 0;
+        this.keys = new Object[3];
+        this.values = new Object[3];
+    }
+
+    public void put(K key, V value) {
+        keys[size] = key;
+        values[size] = value;
+        size++;
+    }
 }
--- HashMap/src/test/java/com/ll/exam/HashMapTest.java
+++ HashMap/src/test/java/com/ll/exam/HashMapTest.java
@@ -1,13 +1,29 @@
 package com.ll.exam;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class HashMapTest {
+    HashMap<String, Integer> hs1;
+    HashMap<String, Object> hs2;
+
+    @BeforeEach
+    void beforeEach(){
+        hs1= new HashMap<>();
+        hs2= new HashMap<>();
+    }
+
     @Test
-    void HashMapTest(){
-        HashMap<String, Integer> hs1= new HashMap<>();
-        HashMap<String, Object> hs2= new HashMap<>();
+    void putTest1(){
+        hs1.put("홍길동",1);
+    }
+
+    @Test
+    void putTest2(){
+        hs2.put("홍길순","배고프다");
     }
 
 }
