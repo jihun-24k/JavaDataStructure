6a9efaf3abb14db59492f5f6712fb724f8f63629
jihun-24k <gi020477@gmail.com>
2022-10-13 11:21:53 +0900

    Chore: LinkedList 세팅

---
+++ LinkedList/src/test/java/org/example/LinkedListTests.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class LinkedListTests {
+    @Test
+    @DisplayName("테스트 실행")
+    public void t1(){
+
+    }
+}
