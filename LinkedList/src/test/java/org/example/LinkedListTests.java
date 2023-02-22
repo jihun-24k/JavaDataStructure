02981392642cf3fa2b0d436b90212cb90d445ecf
jihun-24k <gi020477@gmail.com>
2023-02-22 22:45:10 +0900

    Test: BeforeEach 구문 추가

--- LinkedList/src/test/java/org/example/LinkedListTests.java
+++ LinkedList/src/test/java/org/example/LinkedListTests.java
@@ -1,9 +1,17 @@
 package org.example;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class LinkedListTests {
+    LinkedList linkedList;
+    @Test
+    @BeforeEach
+    public void beforeEach() {
+        linkedList = new LinkedList();
+    }
+
     @Test
     @DisplayName("LinkedList 생성")
     public void createLinkedListTest(){
