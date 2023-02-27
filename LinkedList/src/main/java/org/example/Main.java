cf10d368a7d8fb30d50721fcd80672e29ea89263
jihun-24k <gi020477@gmail.com>
2023-02-27 21:49:47 +0900

    Feat: void add 메서드 노드 링크 구현

--- LinkedList/src/main/java/org/example/Main.java
+++ LinkedList/src/main/java/org/example/Main.java
@@ -9,12 +9,23 @@ public class Main {
 
 class LinkedList {
     private int size;
+    private Node first;
+    private Node last;
 
     public LinkedList() {
         this.size = 0;
     }
 
-    public boolean add(int i) {
+    public boolean add(int e) {
+        final Node l = last;
+        final Node newNode = new Node(l, e, null);
+        last = newNode;
+        if (first == null){
+            first = newNode;
+        }
+        else {
+            l.next = newNode;
+        }
         size++;
         return true;
     }
