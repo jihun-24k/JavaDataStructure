f074872124e65bfdfc6d017b09685072f17544b0
jihun-24k <gi020477@gmail.com>
2023-02-27 21:08:31 +0900

    Test: contains 메서드 테스트 추가

--- LinkedList/src/test/java/org/example/LinkedListTests.java
+++ LinkedList/src/test/java/org/example/LinkedListTests.java
@@ -13,6 +13,7 @@ public class LinkedListTests {
     @BeforeEach
     public void beforeEach() {
         linkedList = new LinkedList();
+        linkedList.add(1);
     }
 
     @Test
@@ -22,16 +23,22 @@ public class LinkedListTests {
     }
 
     @Test
-    @DisplayName("add 메서드 테스트")
-    public void addTest () {
+    @DisplayName("boolean add 메서드 테스트")
+    public void booleanAddTest () {
         boolean isAdd = linkedList.add(1);
         assertTrue(isAdd);
     }
 
     @Test
+    @DisplayName("boolean contains 메서드 테스트")
+    public void containsTest() {
+        boolean hasNode = linkedList.cotains(1);
+        assertTrue(hasNode);
+    }
+
+    @Test
     @DisplayName("리스트 사이즈 테스트")
     public void sizeTest() {
-        linkedList.add(1);
         linkedList.add(2);
         int size = linkedList.size();
         assertEquals(2, size);
