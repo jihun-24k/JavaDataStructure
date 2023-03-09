7b897d106b5f92a7d4264079a90c3080e0777083
jihun-24k <gi020477@gmail.com>
2023-03-09 20:50:37 +0900

    Test: push 테스트 추가

--- Stack/src/test/java/StackTests.java
+++ Stack/src/test/java/StackTests.java
@@ -1,10 +1,24 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StackTests {
     @Test
     @DisplayName("스택 생성 테스트")
     public void createStackTest() {
         Stack stack = new Stack();
+    }
+
+    @Test
+    @DisplayName("push 테스트")
+    public void pushTest() {
+        //given
+        Stack stack = new Stack();
+
+        //when
+        int pushedItem = stack.push(5);
+
+        //then
+        assertEquals(5, pushedItem);
     }
 }
